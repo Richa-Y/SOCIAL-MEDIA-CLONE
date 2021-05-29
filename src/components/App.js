e8a6151637c50734f8b8c15531e91eccfed41b76
Porush Yadav <porushyadav55@gmail.com>
2021-05-29 10:11:34 +0530

    added store

---
+++ src/components/App.js
@@ -0,0 +1,7 @@
+// import "./App.css";
+
+function App() {
+  return <div className="App"> hhhh learnnnnn</div>;
+}
+
+export default App;
---
+++ src/components/test.js
@@ -0,0 +1,6 @@
+export function ShowFavourite(val) {
+  return {
+    type: SHOW_FAVOURITE,
+    val,
+  };
+}
